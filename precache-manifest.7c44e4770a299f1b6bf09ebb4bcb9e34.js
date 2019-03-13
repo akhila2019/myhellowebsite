a23419ebfc4b29d7a906a99323eba472a064a2a1
root <root@ip-172-31-49-37.ec2.internal>
2019-03-13 00:56:12 +0000

    Updates

---
+++ precache-manifest.7c44e4770a299f1b6bf09ebb4bcb9e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myhellowebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6aba36dd998e003f53f2",
+    "url": "/myhellowebsite/static/js/runtime~main.f967e38a.js"
+  },
+  {
+    "revision": "6446d39f09f93e09b7e3",
+    "url": "/myhellowebsite/static/js/main.4955b6dd.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/myhellowebsite/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "6446d39f09f93e09b7e3",
+    "url": "/myhellowebsite/static/css/main.e70dc1f4.chunk.css"
+  },
+  {
+    "revision": "b431fcdaf5fd3f9f9635e71132d09f31",
+    "url": "/myhellowebsite/index.html"
+  }
+];
